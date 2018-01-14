3a1baff85c52d009cb7cdc9a90ccc73d80f2c7ae
Amie Everett <amie.everett@gmail.com>
2018-01-14 17:05:45 -0500

    changing workoutItem

--- dev/scripts/components/workoutItem.js
+++ dev/scripts/components/workoutItem.js
@@ -6,13 +6,10 @@ const WorkoutItem = (props) => {
         const item = props.item;
         const index = item[props.index];
         console.log(index.length);
-
         for (let i=0; i < index.length; i++) {
-            console.log('index[i]', index[i]);
-            {/* <li>{index[i].currentItem}</li> */}
-            {/* <li>{props.item[0].currentItem + ', ' + props.item[0].currentDescription}</li>
-            <li>{props.item[0].currentReps}</li> */}
+            console.log('index[i]', index[i].currentItem);
         }
+        console.log('list',workoutList)
             return (
                 <ul className={`workout flex`}>
                     <li>{date}</li>
