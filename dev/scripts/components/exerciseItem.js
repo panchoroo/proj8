eca657896b7185ed80d6ce7769ccbf1ec08493dd
Amie Everett <amie.everett@gmail.com>
2018-01-15 09:55:51 -0500

    some styling, displaying exercise Items

--- dev/scripts/components/exerciseItem.js
+++ dev/scripts/components/exerciseItem.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 const ExerciseItem = (props) => {
     if (props) {
+        console.log(props);
         const item = props.item;
         console.log('item', item);
+        console.log('desc', props.desc);
+        console.log('reps', props.reps);
         return (
-            <li>{item}</li>
+            <li className='exerciseItem'>
+                <h4>{item}</h4>
+                <h6>{props.desc}</h6>
+                <h6>{props.reps}</h6>
+            </li>
         )
     }
 }
--- dev/scripts/components/workoutItem.js
+++ dev/scripts/components/workoutItem.js
@@ -11,18 +11,21 @@ const WorkoutItem = (props) => {
         const workouts = [];
         
         for (let i=0; i < index.length; i++) {
-            console.log('index[i]', index[i].currentItem);
-            workouts.push(index[i].currentItem);
+            console.log('index[i]', index[i]);
+            workouts.push(index[i]);
         }
             return (
-                <ul className={`workout flex`}>
-                    <li>{date}</li>
-                    {workouts.map((eachEx, index) => {
-                        return <ExerciseItem item={eachEx} date={eachEx} key={index} index={index}/>
-                    // delete= { this.deleteItem } edit={this.editItem}
-                    })}
+                <div className="allWorkouts">
+                    <h2>{date}</h2>
+                    <ul className={`workout flex`}>
+                        {workouts.map((eachEx, index) => {
+                            return <ExerciseItem item={eachEx.currentItem} desc={eachEx.currentDescription}  reps={eachEx.currentReps} date={eachEx} key={index} index={index}/>
+                        // delete= { this.deleteItem } edit={this.editItem}
+                        })}
 
-                </ul>
+                    </ul>
+                </div>
+                
             )  
         }
         
